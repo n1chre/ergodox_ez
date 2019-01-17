548d452fe8ab452f9562d2d555f0cee96bdadd2c
Filip Hrenić <hrenic.filip@gmail.com>
2019-01-17 16:57:50 +0000

    add no_debug and no_print

--- config.h
+++ config.h
@@ -1,3 +1,17 @@
 #pragma once
 
+// disable debugging
+#ifndef NO_DEBUG
+#define NO_DEBUG
+#endif // NO_DEBUG
+
+// disable printing/debugging using hid_listen
+#ifndef NO_PRINT
+#define NO_PRINT
+#endif // NO_PRINT
+
+// how long before oneshot times out
+#ifdef ONESHOT_TIMEOUT
+#undef ONESHOT_TIMEOUT
+#endif // ONESHOT_TIMEOUT
 #define ONESHOT_TIMEOUT 200
