eacfa76e10ea06af5492e2bf68978aa36c2972de
Filip Hrenić <hrenic.filip@gmail.com>
2019-01-17 16:58:05 +0000

    add a few rules for make

--- rules.mk
+++ rules.mk
@@ -1,2 +1,15 @@
+
+# Mouse keys(+4700)
+MOUSEKEY_ENABLE = yes
+
+# Audio control and System control(+450)
+EXTRAKEY_ENABLE = yes
+
+# Console for debug(+400)
+CONSOLE_ENABLE = no
+
+# Commands for debug and configuration
+COMMAND_ENABLE = no
+
 # my keyboard doesn't have lights
 RGBLIGHT_ENABLE = no
